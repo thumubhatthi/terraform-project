73c9de5514f1256e5669f19dbee474d9a733cb07
thumubhatthi <136570439+thumubhatthi@users.noreply.github.com>
2023-08-10 15:23:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 #this file consists of code for instances and sg
 provider "aws" {
-region = "eu-west-3"
-access_key = "AKIA36JX34OE4AXKSK7E"
-secret_key = "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"
+region = "ap-south-1"
+access_key = "AKIASGSZUTFFMFAZYLO2"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-06f7633a6b8e0fb0a"
+  ami             = "ami-0da59f1af71ea4ad2"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "public.pem"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "eu-west-3a"
+  availability_zone = "ap-south-1a"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -25,11 +25,11 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-06f7633a6b8e0fb0a"
+  ami             = "ami-0da59f1af71ea4ad2"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "public.pem"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "eu-west-3b"
+  availability_zone = "ap-south-1b"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -44,22 +44,22 @@ EOF
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-06f7633a6b8e0fb0a"
+  ami             = "ami-0da59f1af71ea4ad2"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "public.pem"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "eu-west-3a"
+  availability_zone = "ap-south-1a"
   tags = {
     Name = "app-server-1"
   }
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-06f7633a6b8e0fb0a"
+  ami             = "ami-0da59f1af71ea4ad2"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "public.pem"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "eu-west-3b"
+  availability_zone = "ap-south-1b"
   tags = {
     Name = "app-server-2"
   }
@@ -90,7 +90,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "rahamshaikterra7gsywgys7889900prodenvgshj"
+  bucket = "baraths3bucket006"
 }
 
 resource "aws_iam_user" "seven" {
@@ -105,9 +105,9 @@ default = ["user1", "user2", "user3", "user4"]
 }
 
 resource "aws_ebs_volume" "eight" {
- availability_zone = "eu-west-3a"
+ availability_zone = "ap-south-1a"
   size = 40
   tags = {
-    Name = "ebs-001"
+    Name = "ebs-barath06"
   }
 }
