e6473b8a067f55c16a8fd7e63624d23132f06444
thumubhatthi <136570439+thumubhatthi@users.noreply.github.com>
2023-08-11 11:38:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 #this file consists of code for instances and sg
 provider "aws" {
-region = "us-east-1"
+region = "eu-west-3"
 access_key = "AKIASGSZUTFFMFAZYLO2"
 secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-08a52ddb321b32a8c"
+  ami             = "ami-07e67bd6b5d9fd892"
   instance_type   = "t2.micro"
-  key_name        = "virgina-key"
+  key_name        = "paris"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-west-3a"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my app created by terraform infrastructurte by raham sir server-1" > /var/www/html/index.html
+echo "hai all this is my app created by terraform infrastructurte by barath sir server-1" > /var/www/html/index.html
 EOF
   tags = {
     Name = "web-server-1"
@@ -25,18 +25,18 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-08a52ddb321b32a8c"
+  ami             = "ami-07e67bd6b5d9fd892"
   instance_type   = "t2.micro"
-  key_name        = "virgina-key"
+  key_name        = "paris"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1b"
+  availability_zone = "eu-west-3b"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
 systemctl start httpd
 chkconfig httpd on
-echo "hai all this is my website created by terraform infrastructurte by raham sir server-2" > /var/www/html/index.html
+echo "hai all this is my website created by terraform infrastructurte by barath sir server-2" > /var/www/html/index.html
 EOF
   tags = {
     Name = "web-server-2"
@@ -44,22 +44,22 @@ EOF
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-08a52ddb321b32a8c"
+  ami             = "ami-07e67bd6b5d9fd892"
   instance_type   = "t2.micro"
-  key_name        = "virgina-key"
+  key_name        = "paris"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-west-3a"
   tags = {
     Name = "app-server-1"
   }
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-08a52ddb321b32a8c"
+  ami             = "ami-07e67bd6b5d9fd892"
   instance_type   = "t2.micro"
-  key_name        = "virgina-key"
+  key_name        = "paris"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1b"
+  availability_zone = "eu-west-3b"
   tags = {
     Name = "app-server-2"
   }
@@ -90,7 +90,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "mybhatthi123"
+  bucket = "pavanbhatthi123"
 }
 
 resource "aws_iam_user" "seven" {
@@ -105,9 +105,9 @@ default = ["user2", "user3", "user4", "user5"]
 }
 
 resource "aws_ebs_volume" "eight" {
- availability_zone = "us-east-1a"
-  size = 40
+ availability_zone = "eu-west-3a"
+  size = 16
   tags = {
-    Name = "ebs-bhatthi09"
+    Name = "ebs-bhatthi08"
   }
 }
